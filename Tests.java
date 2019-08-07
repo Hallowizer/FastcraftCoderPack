48b8271a1f6fac6cfccb404d728e219a76c55ac7
Hallowizer <40325350+Hallowizer@users.noreply.github.com>
2019-08-07 16:25:35 -0700

    Run right up to a wall
    Unfortunately, they hashed their transformation targets, so I need to run another test.

---
+++ Tests.java
@@ -0,0 +1,15 @@
+package fc;
+
+import java.security.MessageDigest;
+
+public class Tests {
+	private static byte[] x;
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println("Algorithm: " + new String(new byte[]{(byte)83, (byte)72, (byte)65, (byte)45, (byte)50, (byte)53, (byte)54}));
+		
+		System.out.println(b("< REDACTED >"));
+	}
+	
+// LINES 14-100 hidden due to copyright
+}
