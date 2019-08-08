2ca4746a7ea43d528caa41a87e60b85fe9bb401f
Hallowizer <40325350+Hallowizer@users.noreply.github.com>
2019-08-07 17:57:48 -0700

    Publish the dehash results

--- fcp/ExitHandler.java
+++ fcp/ExitHandler.java
@@ -5,14 +5,24 @@ import java.util.Map.Entry;
 public class ExitHandler extends Thread {
 	@Override
 	public void run() {
-		System.out.println("Dehash table:");
-		
-		for (Entry<String, String> entry : Transformer.dehashTable.entrySet())
-			System.out.println(entry.getKey() + " -> " + entry.getValue());
-		
-		System.out.println("\nMissing entries:");
-		
-		for (String entry : Transformer.list)
-			System.out.println(entry);
+		try {
+			StringBuffer buf = new StringBuffer();
+			
+			buf.append("\"dehashes\": {\n");
+			
+			for (Entry<String, String> entry : Transformer.dehashTable.entrySet())
+				buf.append("\t\"" + entry.getKey() + "\": \"" + entry.getValue() + "\",\n");
+			
+			buf.append("},\n\"missing-entries\": [\n");
+			
+			for (String entry : Transformer.list)
+				buf.append("\t\"" + entry + "\",\n");
+			
+			buf.append("]\n");
+			
+			System.out.println(buf);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 	}
 }
