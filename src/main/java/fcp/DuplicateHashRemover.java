1fe6e1b9bbfba9208bbf5d8678fe46a547e65857
Hallowizer <40325350+Hallowizer@users.noreply.github.com>
2019-08-18 14:04:13 -0700

    Remove duplicate hashes

---
+++ src/main/java/fcp/DuplicateHashRemover.java
@@ -0,0 +1,54 @@
+package fcp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+
+public class DuplicateHashRemover {
+	public static void main(String[] args) throws Exception {
+		File file = new File(args[0]);
+		
+		byte[] bytes;
+		try (FileInputStream in = new FileInputStream(file)) {
+			bytes = new byte[in.available()];
+			in.read(bytes);
+		}
+		
+		String contents = new String(bytes);
+		Json json = new GsonBuilder().create().fromJson(contents, Json.class);
+		
+		if (json.dehashes == null) {
+			System.out.println("wtf");
+			System.exit(1);
+		}
+		
+		List<String> newMissing = new ArrayList<>();
+		for (String missing : json.missingHashes)
+			if (!newMissing.contains(missing))
+				newMissing.add(missing);
+		
+		System.out.println("\n\n\n\n{\n\t\"dehashes\": {");
+		
+		for (Entry<String, String> entry : json.dehashes.entrySet())
+			System.out.println("\t\t\"" + entry.getKey() + "\": \"" + entry.getValue() + "\",");
+		
+		System.out.println("\t},\n\t\"missing-entries\": [");
+		
+		for (String missing : newMissing)
+			System.out.println("\t\t\"" + missing + "\",");
+		
+		System.out.println("\t]\n}");
+	}
+	
+	private static class Json {
+		public Map<String, String> dehashes;
+		@SerializedName("missing-entries")
+		public String[] missingHashes;
+	}
+}
